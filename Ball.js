63247be9268bc867d757c284185120c82b265637
John Tso <tsotcj@gmail.com>
2022-08-21 20:58:17 -0700

    ping pong game

---
+++ Ball.js
@@ -0,0 +1,61 @@
+const INIT_VEL = 0.025;
+const VEL_INCREASE = 0.00001;
+
+export default class Ball{
+    constructor(ballElem){
+        this.ballElem = ballElem;
+        this.reset();
+    }
+
+    get x(){
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+    }
+
+    set x(value){
+        this.ballElem.style.setProperty("--x", value);
+    }
+
+    get y(){
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+    }
+
+    set y(value){
+        this.ballElem.style.setProperty("--y", value);
+    }
+
+    rect(){
+        return this.ballElem.getBoundingClientRect();
+    }
+
+    reset(){
+        this.x = 50;
+        this.y = 50;
+        this.dir = {x: 0};
+        while (
+            Math.abs(this.dir.x <= .2) 
+            || Math.abs(this.dir.x >= .9)
+        ){
+            const heading = randomNumberBetween(0, 2 * Math.PI);
+            this.dir = {x: Math.cos(heading), y: Math.sin(heading)};
+        }
+        this.vel = INIT_VEL;
+    }
+
+    update(delta){
+        this.x += this.dir.x * this.vel * delta;
+        this.y += this.dir.y * this.vel * delta;
+        this.vel += VEL_INCREASE * delta;
+        const rect = this.rect();
+        if (rect.bottom >= window.innerHeight || rect.top <= 0){
+            this.dir.y *= -1;
+        }
+        
+        // if (rect.right >= window.innerWidth || rect.left <= 0){
+        //     this.dir.x *= -1;
+        // } 
+    }
+}  
+
+function randomNumberBetween(min, max){
+    return Math.random() * (max - min) + min;
+}
---
+++ script.js
@@ -0,0 +1,33 @@
+
+import Ball from "./Ball.js";
+import Paddle from "./Paddle.js"
+const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+
+let lastTime;
+function update(time){
+    if (lastTime != null){
+        const delta = time - lastTime;
+        //update
+        // ball.update(delta);
+        // computerPaddle.update(delta, ball.y);
+        if (isLose()){
+            console.log("lose");
+        }
+
+    }
+    lastTime = time;
+    window.requestAnimationFrame(update);
+}
+
+function isLose(){
+    const rect = ball.rect()
+    return (rect.right >= window.innerWidth || rect.left <= 0);
+}
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
+
+window.requestAnimationFrame(update);
